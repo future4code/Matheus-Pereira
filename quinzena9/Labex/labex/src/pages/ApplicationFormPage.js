c419606cebdcc533e26af8ff34612d0b3eabd08a
mdiasp95 <matheus_dias95@hotmail.com>
2021-11-02 15:14:35 -0300

    criação das páginas

---
+++ quinzena9/Labex/labex/src/pages/ApplicationFormPage.js
@@ -0,0 +1,2 @@
+/*Para o usuário se candidatar à viagens, página que vai ter o formulário de inscrição
+*/
---
+++ quinzena9/Labex/labex/src/pages/HomePage.js
@@ -0,0 +1,2 @@
+/*Para o usuário escolher entre Área Administrativa e Lista de Viagens
+*/
