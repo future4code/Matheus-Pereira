9f3206612f45abcc74383337aba52025270f7dfd
mdiasp95 <matheus_dias95@hotmail.com>
2021-11-04 20:59:24 -0300

    Entrega quinta

--- quinzena9/Labex/labex/src/App.js
+++ quinzena9/Labex/labex/src/App.js
@@ -6,16 +6,23 @@ import HomePage from "./pages/HomePage";
 import ListTripsPage from "./pages/ListTripsPage";
 import LoginPage from "./pages/LoginPage";
 import TripDetailsPage from "./pages/TripDetailsPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (<div className = "APP">
-    <ApplicationFormPage/>
-    <CreateTripPage/>
-    <HomePage/>
-    <AdminHomePage/>
-    <ListTripsPage/>
-    <LoginPage/>
-    <TripDetailsPage/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/formulariodeviagem"  exact element= {<ApplicationFormPage/>}/>
+        <Route path="/criarviagem" exact element = {<CreateTripPage/>} />
+        <Route path="/" exact element={<HomePage/>}/>
+        <Route path="/admin" exact element={<AdminHomePage/>} />
+        <Route path="/listadeviagens" exact element = {<ListTripsPage/>} />
+        <Route path="/login" exact element = {<LoginPage/>} />
+        <Route path="/detalhesdasviagens" exact element = {<TripDetailsPage/>} />
+      </Routes>
+      </BrowserRouter>
+      
     </div>
 
   );
